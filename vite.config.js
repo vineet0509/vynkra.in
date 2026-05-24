7a9fab676c37f8bf2e216f3da2d98b5f4e025409
Vineet Pandey <vineetpandey051996@gmail.com>
2026-05-24 23:29:42 +0530

    Fix Vite manifest output directory by letting laravel-vite-plugin configure it

--- vite.config.js
+++ vite.config.js
@@ -35,9 +35,7 @@ export default defineConfig(({ mode }) => {
         },
 
         build: {
-            manifest: true,
             outDir: 'build',
-            emptyOutDir: true,
         },
 
         base: basePath,
