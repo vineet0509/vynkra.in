12541b0913f7f95b0ce30e8ad1ed3e57d08b52e9
Vineet Pandey <vineetpandey051996@gmail.com>
2026-05-24 23:26:15 +0530

    Configure custom public path for root directory hosting on Hostinger

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -16,3 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+// ✅ ADD THIS LINE (VERY IMPORTANT FOR ROOT DIRECTORY HOSTING)
+$app->usePublicPath(realpath(__DIR__.'/..'));
+
+return $app;
